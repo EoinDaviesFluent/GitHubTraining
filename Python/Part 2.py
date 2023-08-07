8ab53626b90b1bb74662e42a49cace89be1566dd
EoinDaviesFluent <eoindavies@fluentmoney.co.uk>
2023-08-07 12:36:02 +0100

    Intro to Python

---
+++ Python/Part 2.py
@@ -0,0 +1,3 @@
+name=input('Please enter your username: ')
+age=input('Please enter your age: ')
+print (name,'is',age,'years old')
---
+++ Python/Part 3.py
@@ -0,0 +1,7 @@
+width=float(input('Please enter the width of the rectangle: '))
+length=float(input('Please enter the length of the rectangle: '))
+perimeter=(width*2)+(length*2)
+area=width*length
+print ('A rectange which is',width,'by',length,':')
+print ('Has a perimeter of',perimeter)
+print ('And a area of',area)
